de5b14e4e60ae41e350f32a11dc0fd3516994aba
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-10-09 19:18:22 +0530

    image added

---
+++ app/test2/page.tsx
@@ -0,0 +1,8 @@
+import PropertyRegistrationForm from "@/components/Form/registerproperty";
+import HeroSection from "@/components/HeroSection";
+import SearchFilter from "@/components/ui/filter";
+import React from "react";
+
+export default function page() {
+  return <div className="">hello</div>;
+}
